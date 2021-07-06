8543cbeb2093591215158aa6275572d2aad513f6
ihab abadi <utopios@imac-de-ihab.home>
2021-07-06 16:11:57 +0200

    debut formation android

---
+++ partie-2/app/src/main/java/formation/kotlin/androidapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package formation.kotlin.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var text = TextView(this)
+        text.text = "TextView created by code"
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
