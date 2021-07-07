da76df0b2f69f9c4bdfbb31b7ee04b20bd73aae6
ihab abadi <utopios@imac-de-ihab.home>
2021-07-07 15:36:11 +0200

    webview debut

--- partie-2/app/src/main/java/formation/kotlin/androidapp/SecondActivity.kt
+++ partie-2/app/src/main/java/formation/kotlin/androidapp/SecondActivity.kt
@@ -2,10 +2,13 @@ package formation.kotlin.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import formation.kotlin.mylibrary.BridgeGameActivity
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : BridgeGameActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        val b1 = findViewById<Button>(R.id.bridge_game_b1)
+        b1.text = "new Text"
     }
 }
--- partie-2/mylibrary/src/main/java/formation/kotlin/mylibrary/BridgeGameActivity.kt
+++ partie-2/mylibrary/src/main/java/formation/kotlin/mylibrary/BridgeGameActivity.kt
@@ -1,11 +1,41 @@
 package formation.kotlin.mylibrary
 
+import android.content.DialogInterface
+import android.icu.text.LocaleDisplayNames
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_bridge_game.*
 
-class BridgeGameActivity : AppCompatActivity() {
+open class BridgeGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bridge_game)
+        bridge_game_webview.loadUrl("https://google.com")
+        bridge_game_webview.settings.javaScriptEnabled = true
+        bridge_game_webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                view!!.loadUrl(request!!.url.toString())
+                return true
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if(keyCode == KeyEvent.KEYCODE_BACK && bridge_game_webview.canGoBack()){
+            //AlertDialog
+                val dialog = AlertDialog.Builder(this)
+                dialog.setMessage("Vous êtes sur de quitter ?").setPositiveButton("OK",DialogInterface.OnClickListener {dialog, id -> bridge_game_webview.goBack()})
+                    .setNegativeButton("Non", DialogInterface.OnClickListener {dialog, id -> false})
+                dialog.create().show()
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
